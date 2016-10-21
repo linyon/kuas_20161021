3de0a11a42521d434873f28239dabdda88e2e43c
linyon <linyon@DESKTOP-N4MDUS5>
2016-10-21 10:58:55 +0800

    update

---
+++ 20161021/20161021/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20161021.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+             return View();
+           // return Content("<h1>yon is good</h1>");
+        }
+    }
+}
---
+++ 20161021/20161021/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_20161021.MvcApplication" Language="C#" %>
